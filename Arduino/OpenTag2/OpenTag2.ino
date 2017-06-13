2ff1d20021a11a3e97c8ff8020f3d7fd3fc461d6
davidallenmann <dm@humu.io>
2017-06-13 19:17:30 -0400

    initial commit

---
+++ Arduino/OpenTag2/OpenTag2.ino
@@ -0,0 +1,20 @@
+// Copyright Loggerhead Instruments, 2017
+// David Mann
+
+// OpenTag2 is an underwater motion datalogger
+// designed around the ATSAMD21G that is used by the Arduino Zero
+
+// OpenTag2 supports the following sensors:
+// Pressure/Temperature
+// IMU
+// RGB light
+// GPS
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
