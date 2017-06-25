04c6854c45cc7f88c1a20916265b72f06720be0d
davidallenmann <dm@humu.io>
2017-06-25 11:31:25 -0400

    euler calculations

---
+++ Arduino/OpenTag2/euler.ino
@@ -0,0 +1,31 @@
+
+
+// simple calculation of pitch, roll, yaw
+void euler(){
+  // roll
+  float phi = atan2(accel_y, accel_z); // roll in radians
+
+  // de-rotate by roll angle
+  float sinAngle = sin(phi);
+  float cosAngle = cos(phi);
+  float Bfy = (mag_y * cosAngle) - (mag_z * sinAngle);
+  float Bz = (mag_y * sinAngle) + (mag_z * cosAngle);
+  float Gz = (accel_y * sinAngle) + (accel_z * cosAngle);
+
+  // theta = pitch angle
+  float theta = atan(- accel_x / Gz);
+  sinAngle = sin(theta);
+  cosAngle = cos(theta);
+
+  // de-rotate by pitch angle theta
+  float Bfx = (mag_x * cosAngle) + (Bz * sinAngle);
+  float Bfz = (-mag_x * sinAngle) + (Bz * cosAngle);
+
+  // Psi = yaw = heading
+  float psi = atan2(-Bfy, Bfx) + (PI/2.0);
+
+  pitch = degrees(theta);
+  roll = degrees(phi);
+  yaw = degrees(psi);
+}
+
